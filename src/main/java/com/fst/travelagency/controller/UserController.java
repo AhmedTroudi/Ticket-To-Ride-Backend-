2ece22c42e603b18a3f553a89c3a86bdedd0ce07
AhmedTroudi <ahmed.troudi@etudiant-fst.utm.tn>
2019-01-03 22:28:43 +0100

    commit

--- src/main/java/com/fst/travelagency/controller/UserController.java
+++ src/main/java/com/fst/travelagency/controller/UserController.java
@@ -4,7 +4,6 @@ package com.fst.travelagency.controller;
 import com.fst.travelagency.model.User;
 import com.fst.travelagency.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
