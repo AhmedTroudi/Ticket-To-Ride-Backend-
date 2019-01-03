be30c8d77749fb653e23a23984fe011dc81b782c
AhmedTroudi <ahmed.troudi@etudiant-fst.utm.tn>
2019-01-03 21:32:26 +0100

    tweaking security

--- src/main/java/com/fst/travelagency/controller/UserController.java
+++ src/main/java/com/fst/travelagency/controller/UserController.java
@@ -4,10 +4,12 @@ package com.fst.travelagency.controller;
 import com.fst.travelagency.model.User;
 import com.fst.travelagency.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -19,15 +21,15 @@ public class UserController {
         this.repository = repository;
     }
 
-    // Get all users
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
-        return repository.findAll();
+        return repository.findAll().stream()
+                .collect(Collectors.toList());
     }
 
-    // login
+
     @PostMapping("/users/{id}")
-    public Optional<User> findUserById(@RequestBody User data,  @PathVariable long id) {
+    public Optional<User> findUserById(@PathVariable long id) {
 
         Optional<User> user =  repository.findById(id);
         if ((user!=null)){
@@ -36,8 +38,6 @@ public class UserController {
         return null;
     }
 
-
-    // Update a User
     @PutMapping("/users/update/{id}")
     public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable long id) {
         Optional<User> userOptional = repository.findById(id);
@@ -48,7 +48,6 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
-    //Delete a User
     @DeleteMapping("/users/delete/{id}")
     public void deleteUser(@PathVariable long id) {
         repository.deleteById(id);
